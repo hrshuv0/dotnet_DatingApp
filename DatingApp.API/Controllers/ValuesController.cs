a239e80cce84c92f6ba580cf37f395223fb455ab
shuvo <hrshuva9@gmail.com>
2022-02-17 23:31:22 +0600

    added security - api
    created login and register service, added validation in the API, token authentication, created register and login method , added authentication middle-ware.

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -1,11 +1,12 @@
-using System.Linq;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : Controller
@@ -27,6 +28,7 @@ namespace DatingApp.API.Controllers
         }
 
 
+        [AllowAnonymous]
         [HttpGet("{id}")]
         public async Task<IActionResult> GetValue(int id)
         {
