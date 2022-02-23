a15b895433c1070a8ef176403640ea0a1e2e90f0
shuvo <hrshuva9@gmail.com>
2022-02-23 15:30:11 +0600

    added add photo component - spa
    added feature to add photo from edit photo

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -28,6 +28,8 @@ import { MemberListResolver } from './_resolvers/member-list.resolver copy';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { MemberEditResolver } from './_resolvers/member-edit.resolver copy';
 import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
+import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
+import { FileUploadModule } from 'ng2-file-upload';
 
 export function tokenGetter(){
   return localStorage.getItem('token');
@@ -52,7 +54,10 @@ export class CustomHammerConfig extends HammerGestureConfig{
     MessagesComponent,
     MemberCardComponent,
     MemberDetailComponent,
-    MemberEditComponent
+    MemberEditComponent,
+    PhotoEditorComponent
+    
+    
   ],
   imports: [
     BrowserModule,
@@ -63,6 +68,7 @@ export class CustomHammerConfig extends HammerGestureConfig{
     RouterModule.forRoot(appRoutes),
     NgxGalleryModule,
     BrowserAnimationsModule,
+    FileUploadModule,
 
 
     JwtModule.forRoot({
